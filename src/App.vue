f4989107131bcd3da652f0c033808dd1260805a7
Aldahir Pulido <aldahipr98@gmail.com>
2023-07-23 18:12:51 -0600

    Adding project files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <!-- SIDEBAR -->
+    <side-bar
+      @selectOption="onSelectOption"
+    />
+
+    <v-main>
+      <!-- CARDS -->
+      <image-gallery :isWithColor="isWithColor"/>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import SideBar from '@/components/SideBar'
+  import ImageGallery from './components/ImageGallery'
+  import {ref} from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  const isWithColor = ref(true)
+
+  const onSelectOption = (isColor) => {
+    isWithColor.value =  isColor
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+            title="Juan López"
+            subtitle="juan_lopez@gmail.com"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item 
+            prepend-icon="mdi-image" 
+            title="Color Gallery" 
+            value="color"
+            @click="selectOption(true)"
+          />
+          <v-list-item 
+            prepend-icon="mdi-invert-colors" 
+            title="Grayspace Gallery" 
+            value="grayscale"
+            @click="selectOption(false)"
+        />
+          
+        </v-list>
+      </v-navigation-drawer>
+</template>
+
+<script setup>
+    import { defineEmits } from 'vue';
+
+    const emits = defineEmits(['selectOption'])
+
+    const selectOption = (isColor) => {
+        emits("selectOption", isColor)
+    }
+</script>
